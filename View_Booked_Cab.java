b6fa911e60fee4aba2da5c1661018ab4315da381
SananKHAN9579 <115710247+SananKHAN9579@users.noreply.github.com>
2023-02-17 19:46:05 +0530

    Add files via upload
    Viewing Booked cabs

---
+++ View_Booked_Cab.java
@@ -0,0 +1,54 @@
+
+package CAB_BOOKING;
+
+        import javax.swing.*;
+        import java.awt.*;
+        import java.sql.ResultSet;
+
+public class View_Booked_Cab extends JFrame {
+    Font f;
+    JTable t1;
+    String x[] = {"Book ID","Username","Driver Name","Car","Source","Destination","Price"};
+    String y[][] = new String[20][7]; // 20 column 7 rows
+    int i = 0, j = 0;
+    View_Booked_Cab()
+    {
+
+        super("All Booked Cab Record");
+        setSize(1300,400);
+        setLocation(0,10);
+        f = new Font("MS UI Gothic",Font.BOLD,17);
+
+        try {
+            Connection_Class obj = new Connection_Class();
+            String q = "select * from intracitycab";
+            ResultSet rest = obj.stmt.executeQuery(q);
+
+            while (rest.next()){
+                y[i][j++]=rest.getString("BookId");
+                y[i][j++]=rest.getString("UserName");
+                y[i][j++]=rest.getString("DriverName");
+                y[i][j++]=rest.getString("Car");
+                y[i][j++]=rest.getString("source");
+                y[i][j++]=rest.getString("Destination");
+                y[i][j++]=rest.getString("Price");
+                i++;
+                j=0;
+            }
+            t1 = new JTable(y,x);
+        }catch (Exception re){
+            re.printStackTrace();
+        }
+
+        t1.setFont(f);
+        t1.setBackground(Color.BLACK);
+        t1.setForeground(Color.WHITE);
+
+        JScrollPane js = new JScrollPane(t1); // use to scroll the information table
+        add(js);
+    }
+
+    public static void main(String[] args) {
+        new View_Booked_Cab().setVisible(true);
+    }
+}
